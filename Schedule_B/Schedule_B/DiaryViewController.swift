b73453b26ffcf17558d4a248a7eef5a83294abb3
KEEN <homurahomu@naver.com>
2021-02-09 14:58:10 +0900

    initial storyboard

---
+++ Schedule_B/Schedule_B/DiaryViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Schedule_B
+//
+//  Created by KEEN on 2021/02/08.
+//
+
+import UIKit
+import FSCalendar
+
+class DiaryViewController: UIViewController {
+
+  @IBOutlet weak var diaryCalendarView: FSCalendar!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    diaryCalendarView.backgroundColor = UIColor(red: 241/255, green: 249/255, blue: 255/255, alpha: 1)
+    diaryCalendarView.appearance.selectionColor = UIColor(red: 38/255, green: 153/255, blue: 251/255, alpha: 1)
+    diaryCalendarView.appearance.todayColor = UIColor(red: 188/255, green: 224/255, blue: 253/255, alpha: 1)
+    
+    diaryCalendarView.scrollEnabled = true
+    diaryCalendarView.scrollDirection = .vertical
+  }
+
+}
+
---
+++ Schedule_B/Schedule_B/MenuTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MenuTableViewCell.swift
+//  Schedule_B
+//
+//  Created by KEEN on 2021/02/09.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+  @IBOutlet weak var menuButton: UIButton!
+}
